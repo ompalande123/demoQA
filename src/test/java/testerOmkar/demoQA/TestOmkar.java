796e69a0491695ac5c6f9c982a47e6c01a22b46f
ompalande123 <ompalande123@gmail.com>
2024-07-21 15:36:36 +0530

    Omkar: First commit...

---
+++ src/test/java/testerOmkar/demoQA/TestOmkar.java
@@ -0,0 +1,26 @@
+package testerOmkar.demoQA;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestOmkar {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\mySeleniumDrivers\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://demoqa.com");
+		
+		Thread.sleep(5000);
+		
+		driver.quit();
+		
+		
+		
+		
+	}
+
+}
